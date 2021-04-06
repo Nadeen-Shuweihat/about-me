c5ff7a71667ed62227ed1f3fa69cc5f440646fca
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-04-06 22:30:47 +0300

    added more questions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# about-me
-guessing game for lab02
+# lab 03
+
+### About me guessing game
+
+i buit a simple website about me. it will tell you some basic information about me , how do i look like and top 10 songs for me. But before entering you will have a humble notes from me and it includes some questions you have to answer 
+
+And yes, without a css for now because we all should enjoy the simple things in life hahahah
+
+#### Author : Nadeen Shuweihat
--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 'use strict'; 
 
-let userName = prompt('What is your name?');
+alert ('Hello there !')
+alert ('You will see a work that tells the moon MOVE i will sit in your place ')
+
+alert('just kidding,let\'s start by knowing who are you first');
+
+let score = 0;
+
+let userName = prompt('So, first things first What is your name?');
 // console.log(userName);
 alert('Hey there ' + userName);
 
@@ -10,6 +17,7 @@ switch(userAnswer.toLowerCase()){
     case 'y':
     case 'yes':
         alert('Amaazing because i forget who i am sometimes') 
+    score++;    
     break;
     case 'n':
     case 'no':
@@ -19,7 +27,7 @@ switch(userAnswer.toLowerCase()){
         alert('okai '+ userAnswer);  
 }
 
-alert(userName +', i will ask you now 4 random yes or no question. Be preapared')
+alert(userName +', i will ask you now 6 random yes or no question and i will give you a score to each correct answer. Be preapared')
 
 let userQues1 = prompt('Do i believe in ghosts?')
 // console.log(userQues1)
@@ -27,6 +35,7 @@ switch(userQues1.toLowerCase()){
     case 'y':
     case 'yes':
         alert('I think i do') 
+    score++;    
     break;
     case 'n':
     case 'no':
@@ -46,6 +55,7 @@ switch(userQues2.toLowerCase()){
     case 'n':
     case 'no':
         alert('you are right')  
+    score++;    
     break;
     default:
         alert('i was born in 1998,calculate it');  
@@ -61,7 +71,8 @@ switch(userQues3.toLowerCase()){
     break;
     case 'n':
     case 'no':
-        alert('you are right')  
+        alert('you are right')
+    score++;      
     break;
     default:
         alert('i know it is a silly');  
@@ -74,6 +85,7 @@ switch(userQues4.toLowerCase()){
     case 'y':
     case 'yes':
         alert('childesh i know') 
+    score++;    
     break;
     case 'n':
     case 'no':
@@ -83,4 +95,54 @@ switch(userQues4.toLowerCase()){
         alert('i know it is silly ');  
 }
 
-alert('hope you enjoyed our questions '+ userName);
+let userQues5 = parseInt(prompt('what is my hight'));
+for(let i =1; i<4;i++){
+    if(userQues5 === 154){
+        alert('right');
+        score++;
+        break;
+    }else if(userQues5 > 154){
+        console.log(userQues5);
+        userQues5=parseInt(prompt('your guess is too high try again'));
+    }else if(userQues5 < 154){
+        userQues5=parseInt(prompt('your guess is too low try again'));
+    }
+}
+ alert('the right answer is 154');
+
+ let myFav = ['sabrina','you will never get rich','roman holiday','bewitched'];
+
+ 
+
+ for(let i=0; i<6; i++){
+    let favMovie = prompt('can you guess my favorite movies?').toLocaleLowerCase();
+      if (favMovie===myFav[0] || favMovie===myFav[1] || favMovie===myFav[2] || favMovie===myFav[3]){
+          alert('corret ,i see you are a fan to oldies like me ');
+          score++;
+          break;
+      }else ('nope, try getting back in time with the guessing');
+ }
+
+ /*for(let n=0; n<6 ; n++){
+     let guess = prompt('can you guess my favorite movies?').toLocaleLowerCase();
+     for(let j=0; j<myFav.length;j++){
+         if(myFav.includes(guess)){
+             alert('yeaay you got it right');
+             i=6
+             break
+         }
+     }
+     if(n !==6){
+         alert('nop ,guess again');
+     }
+ }*/ // i like it more than the first way 
+
+ alert('My favorite movies are sabrina 1954, you will never get rich 1941, roman holiday 1953,bewitched 1964')
+
+ //if((parseInt(userQues5)=== 154)){alert('you are corresct');}
+     // else if (for()) - was trying to solve it by nesting the for in the if 
+
+
+alert ('your scores is ' +score);
+
+alert( 'hope you enjoyed the questions '+ userName +' نزلن عيوني لانو منهم');
